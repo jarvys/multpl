dfe6253bbd9d0ea5bc2932031d34883ce03ef75e
Jarvys <yuhan534@126.com>
2014-08-25 19:27:33 +0800

    hotfix

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var template = require('underscore').template;
 var multiline = require('multiline');
 
 module.exports = function(fn) {
-	return _.template(multiline(fn).trim());
+	return template(multiline(fn).trim());
 }
 
